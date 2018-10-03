fe18953b383f5a7135f1ec985f1179c42a59b912
adamsnr1 <36975465+SirWashnerQuant@users.noreply.github.com>
2018-10-03 09:43:24 -0400

    second attempt at collision avoidance
    we will see how well we can polish, still breaks when coming directly into bottom and top for some reason..

--- src/EnemyMageM.java
+++ src/EnemyMageM.java
@@ -13,6 +13,7 @@ public class EnemyMageM extends Creature{
 	private Animation animRight;
 	private Animation animStill;
 	private Animation currentAnim;
+	private Rectangle left, right, up, down;
 	private int direction;
 	private int num;
 	private int forceDir;
@@ -20,9 +21,10 @@ public class EnemyMageM extends Creature{
 	private int steeringTime;
 	private float centerX;
 	private float centerY;
+	private boolean xStopped, yStopped, stopped;
 	private float distanceNeed;
 	private int ticksNeed;
-	private Entity immediateBlocker;
+	private Entity pathBlocker;
 	private float offsetX, offsetY;
 	private boolean tracking = true;
 	private Point ahead = new Point();
@@ -51,11 +53,18 @@ public class EnemyMageM extends Creature{
 		animLeft = new Animation(250, Assets.mageM_left);
 		animRight = new Animation(250, Assets.mageM_right);
 		animStill = new Animation(250, Assets.mageM_idle);
+		
+		left = new Rectangle( (int) this.x - 20, (int) this.y, 20, bounds.height);
+		right = new Rectangle( (int) this.x + bounds.width, (int) this.y, 20, bounds.height );
+		up = new Rectangle( (int) this.x, (int) this.y - 20, bounds.width, 20);
+		down = new Rectangle( (int) this.x, (int) this.y + bounds.height, bounds.width, 20);
 	
 	}
 
 	@Override
 	public void tick() {
+		
+		updateBoxes();
 		
 		
 		offsetX = handler.getGameCamera().getxOffset();
@@ -90,14 +99,18 @@ public class EnemyMageM extends Creature{
 			currentAnim = animStill;
 		}
 		
-		
+		npcWanderLogic();
 		collisionLogic();
-		npcTrackLogic();
+		if(!stopped) {
+			npcTrackLogic();
+		} else {
+			findEntity();
+		}
 		checkBounds();
 		attack();
 		checkPlayer();
 		move();
-		npcWanderLogic();
+		
 		
 		
 	}
@@ -124,9 +137,37 @@ public class EnemyMageM extends Creature{
 			g.drawRect((int)(handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(0, 0).x - handler.getGameCamera().getxOffset()), (int) (handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(0, 0).y - handler.getGameCamera().getyOffset()), handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(0, 0).width, handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(0, 0).height);
 		}
 		
+		g.drawRect(left.x, left.y, left.width, left.height);
+		g.drawRect(right.x, right.y, right.width, right.height);
+		g.drawRect(up.x, up.y, up.width, up.height);
+		g.drawRect(down.x, down.y, down.width, down.height);
+			
+	}
+	
+	
+	private void updateBoxes() {
+		
+		left.x = (int) (this.x - 20 - offsetX); 
+		left.y = (int) (this.y - offsetY);
+		left.width = 20;
+		left.height = bounds.height;
+		
+		right.x = (int) (this.x + bounds.width - offsetX); 
+		right.y = (int) (this.y - offsetY);
+		right.width = 20;
+		right.height = bounds.height;	
+		
+		up.x = (int) (this.x - offsetX); 
+		up.y = (int) (this.y - 20 - offsetY);
+		up.width = bounds.width;
+		up.height = 20;
+		
+		down.x = (int) (this.x - offsetX); 
+		down.y = (int) (this.y + bounds.height - offsetY);
+		down.width = bounds.width;
+		down.height = 20;
 		
 		
-		//g.drawRect( (int) (walkingArea.x - handler.getGameCamera().getxOffset()),(int) (walkingArea.y - handler.getGameCamera().getyOffset()), walkingArea.width, walkingArea.height);
 	}
 	
 	public void attack() {
@@ -139,20 +180,123 @@ public class EnemyMageM extends Creature{
 		
 	}
 	
+	private void findEntity() {
+		
+		if(xStopped) {
+			
+			//stopped when moving to right
+			if(direction == 0) {
+				
+				for(int i = 0; i < handler.getWorld().getEntityManager().getEntities().size(); i++) {
+					
+					if(right.intersects(handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(-offsetX, -offsetY))) {
+						pathBlocker = handler.getWorld().getEntityManager().getEntities().get(i);
+						avoidance(pathBlocker);
+					}
+					
+				}
+				
+				
+			//stopped when moving to left
+			} else if (direction == 1) {
+				
+				for(int i = 0; i < handler.getWorld().getEntityManager().getEntities().size(); i++) {
+					
+					if(left.intersects(handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(-offsetX, -offsetY))) {
+						pathBlocker = handler.getWorld().getEntityManager().getEntities().get(i);
+						avoidance(pathBlocker);
+					}
+				}
+				
+			} else {
+				
+				System.out.println("What?");
+				
+			}
+			
+			
+		} else {
+			
+			//stopped while going up
+			if(direction == 2) {
+				
+				for(int i = 0; i < handler.getWorld().getEntityManager().getEntities().size(); i++) {
+					
+					if(up.intersects(handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(-offsetX, -offsetY))) {
+						pathBlocker = handler.getWorld().getEntityManager().getEntities().get(i);
+						avoidance(pathBlocker);
+						
+					}
+					
+				}
+				
+				
+			//stopped while going down
+			} else if (direction == 3) {
+				
+				for(int i = 0; i < handler.getWorld().getEntityManager().getEntities().size(); i++) {
+					
+					if(down.intersects(handler.getWorld().getEntityManager().getEntities().get(i).getCollisionBounds(-offsetX, -offsetY))) {
+						pathBlocker = handler.getWorld().getEntityManager().getEntities().get(i);
+						avoidance(pathBlocker);
+					}
+					
+				}
+				
+				
+			}			
+			
+			
+		}
+		
+	}
+	
+	private void avoidance(Entity e) {
+				
+		System.out.println(e);
+		
+	}
+	
 	private void collisionLogic() {
 		
+		moveTimer += System.currentTimeMillis() - lastMove;
+		lastMove = System.currentTimeMillis();		
 		
+		if(xMove != 0 && lastX == this.x) {
+			xStopped = true;
+		} else {
+			xStopped = false;
+			
+		}
 		
+		if(yMove != 0 && lastY == this.y) {
+			yStopped = true;
+			
+		} else {
+			yStopped = false;
+			
+		}
+		
+		if(yStopped || xStopped) {
+			stopped = true;
+		} else {
+			stopped = false;
+		}
+		
+		if(moveTimer < moveCooldown) {
+			return;
+		}
+		
+		lastX = this.x;
+		lastY = this.y;
+		
+		moveTimer = 0;
 		
 		
 	}
 
 	
 	private void npcTrackLogic(){
-		
-		if(!tracking) {
-			return;
-		}
 		
 		if(this.x < handler.getWorld().getEntityManager().getPlayer().x) {
 			if(Math.abs(this.x - handler.getWorld().getEntityManager().getPlayer().x) < 5) {
@@ -191,22 +335,10 @@ public class EnemyMageM extends Creature{
 			
 		}
 		
-		
-		
 	}
 	
 	private void npcWanderLogic(){
-		moveTimer += System.currentTimeMillis() - lastMove;
-		lastMove = System.currentTimeMillis();		
 		
-		
-		
-		if(moveTimer < moveCooldown) {
-			return;
-		}
-		
-		
-		moveTimer = 0;
 
 	}
 	
